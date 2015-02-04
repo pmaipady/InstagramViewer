fe8b30ceb83c331d5df210ae292f7266c75d9e0d
pmaipady <prithvirajm2@yahoo.com>
2015-02-03 19:59:43 -0800

    Initial version

---
+++ app/src/main/java/com/example/prit/instagramviewer/InstagramPhoto.java
@@ -0,0 +1,13 @@
+package com.example.prit.instagramviewer;
+
+/**
+ * Created by Prit on 2/3/2015.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageurl;
+    public int imageheight;
+    public int likesCount;
+
+}
---
+++ app/src/main/java/com/example/prit/instagramviewer/InstagramPhotosAdapter.java
@@ -0,0 +1,39 @@
+package com.example.prit.instagramviewer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by Prit on 2/3/2015.
+ */
+public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
+
+    public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        InstagramPhoto photo = getItem(position);
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.items_photo,parent,false);
+
+        }
+        TextView tvCaption = (TextView)convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView)convertView.findViewById(R.id.ivPhoto);
+        tvCaption.setText(photo.caption);
+        ivPhoto.setImageResource(0);
+        Picasso.with(getContext()).load(photo.imageurl).into(ivPhoto);
+        return convertView;
+    }
+}
